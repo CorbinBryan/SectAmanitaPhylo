21499a86cd344fd15b12fa3bfdff0924c2306750
CorbinBryan <bryan7@wisc.edu>
2023-02-13 12:50:15 -0600

    modified:   bash/genbank_dl.sh

--- bash/genbank_dl.sh
+++ bash/genbank_dl.sh
@@ -2,9 +2,8 @@
 # A script to download files from genbank
 
 file=$1
-name=$2 
 
 $(cat ~/SectAmanitaPhylo/RawData/GB_acc_no/${file}) | while read i; do
     esearch -db nucleotide -query "$i" | efetch -format fasta
-done >> ~/SectAmanitaPhylo/DerivedData/${name}.fasta
+done >> ~/SectAmanitaPhylo/DerivedData/${file}.fasta
 
