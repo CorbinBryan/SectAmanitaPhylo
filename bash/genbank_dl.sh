20dae42c66dd1305f8ba90fd0cc2b23b309f62eb
CorbinBryan <bryan7@wisc.edu>
2023-02-13 14:35:08 -0600

    modified:   RawData/GB_acc_no/BTUB_GenBank 	modified:   RawData/GB_acc_no/EF1-A_GenBank 	modified:   RawData/GB_acc_no/ITS_GenBank 	modified:   RawData/GB_acc_no/LSU_GenBank 	modified:   bash/genbank_dl.sh

--- bash/genbank_dl.sh
+++ bash/genbank_dl.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# A script to download files from genbank
+# A script to download files from genbank given accession nos. 
 
-file=$1
-
-$(cat ~/SectAmanitaPhylo/RawData/GB_acc_no/${file}) | while read i; do
-    esearch -db nucleotide -query "$i" | efetch -format fasta
-done >> ~/SectAmanitaPhylo/DerivedData/${file}.fasta
-
+for FILE in $(ls ~/SectAmanitaPhylo/RawData/GB_acc_no); do 
+#    touch ${FILE}.fa
+    epost -db nuccore -input ~/SectAmanitaPhlyo/RawData/GB_acc_no/${FILE} -format acc | efetch -format fasta > ${FILE}.fa
+    if 
+done 
