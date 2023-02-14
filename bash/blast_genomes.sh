4a58754eaec85c22740745764f096cdff7998c4d
CorbinBryan <bryan7@wisc.edu>
2023-02-13 20:54:43 -0600

    forgot to save before I commited, my bad

--- bash/blast_genomes.sh
+++ bash/blast_genomes.sh
@@ -5,13 +5,20 @@
 
 FILE=$1 
 
-if ![ -d ./blastOut ]; then
+if [ ! -d ./blastOut ]; then
     mkdir ./blastOut; 
-elif ![-d ./assemblies]; then
+elif [ ! -d miniconda3 ]; then
+    cp /staging/bryan7/blast.tar.gz .
+    chmod 644 blast.tar.gz
+    tar -zxvf blast.tar.gz
+elif [ ! -d ./assemblies]; then
     tar -zxvf assemblies.tar.gz
 else 
     makeblastdb -in ${FILE} -dbtype nucl
     blastn -db ./${FILE} -query 4Loci_for_blast.fa -outfmt 6 -max_target_seqs 2 -num_threads 4 -out ./blastOut/${FILE}_hits.txt
 fi
 
-gzip ./blastOut
+if [ $(ls ./blastOut | wc -l) == 25 ]; then
+    tar -czvf blastOut.tar.gz ./blastOut
+    mv blastOut.tar.gz /staging/bryan7/
+fi
