d88a85a2de3007d6ef3132968816e58a079467dc
CorbinBryan <bryan7@wisc.edu>
2023-02-13 20:53:58 -0600

    updated blast_genomes.sh to reflect being run in condor environment. Also, confirmed that genbank_dl.sh works, downloaded fastas.

--- bash/blast_genomes.sh
+++ bash/blast_genomes.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # a script to blast seq from four loci against 25 genomes
 # note: most unzip your assembly and have in subdirectory 
-# either remove the loop or specify 100 cores, 4/genome
-mkdir ./blastOut
+# Chris, if you're reading this then change 4Loci_for_blast.fa to [MATINGTYPES].fa
 
-for FILE in ./assemblies; do 
+FILE=$1 
+
+if ![ -d ./blastOut ]; then
+    mkdir ./blastOut; 
+elif ![-d ./assemblies]; then
+    tar -zxvf assemblies.tar.gz
+else 
     makeblastdb -in ${FILE} -dbtype nucl
-    blastn -db ./${FILE} -query Amu_4_loci.fa -outfmt 6 -max_target_seqs 2 -num_threads 4 -out ./blastOut/${FILE}_hits.txt
-done 
+    blastn -db ./${FILE} -query 4Loci_for_blast.fa -outfmt 6 -max_target_seqs 2 -num_threads 4 -out ./blastOut/${FILE}_hits.txt
+fi
 
 gzip ./blastOut
