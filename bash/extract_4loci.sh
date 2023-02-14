5825cf9a47ba426dbe6db810cfaac3432b32728a
CorbinBryan <bryan7@wisc.edu>
2023-02-14 14:16:54 -0600

    updated extractionscript

--- bash/extract_4loci.sh
+++ bash/extract_4loci.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #extract four loci from given genomes
 
-mkdir ./Genome_fastas 
-while read FILE; do
-    for ROW in $(cat ./blastOut/${FILE}_hits.txt | wc -l | seq); do
-        ENTR=$(awk -v awkvar="$ROW" 'FNR == ROW {print $2}' ./blastOut/${FILE}_hits.txt)
-        START=$(awk -v awkvar="$ROW" 'FNR == ROW {print $9}' ./blastOut/${FILE}_hits.txt)
-        STOP=$(awk -v awkvar="$ROW" 'FNR == ROW {print $10}' ./blastOut/${FILE}_hits.txt)
+if [ ! -d ./Genome_fastas ]; then
+mkdir ./Genome_fastas
+fi
+
+for FILE in $(cat blast_names.txt); do
+    for i in $(seq $(cat ./blastOut/${FILE}_hits.txt | wc -l)); do
+        ENTR=$(awk -v awkvar="$i" 'FNR == i {print $2}' ./blastOut/${FILE}_hits.txt)
+        START=$(awk -v awkvar="$i" 'FNR == i {print $9}' ./blastOut/${FILE}_hits.txt)
+        STOP=$(awk -v awkvar="$i" 'FNR == i {print $10}' ./blastOut/${FILE}_hits.txt)
+        RANGE=$(awk -v awkvar="$i" 'FNR == i {print $9"-"$10}')
+        echo $RANGE
         if [[ $STOP -gt $START ]]; then
-            STR="+"
-            RANGE=$(echo "$START""-""$STOP")
+            STR="plus"
         else
-            STR="-"
-            RANGE=$(echo "$STOP""-""$START")
+            STR="minus"
         fi 
-        blastdbcmd -db ./assemblies/${FILE} -dbtype nucl -entry "$ENTR" -range "$RANGE" -strand "$STR" -out 
+        blastdbcmd -db ./assemblies/${FILE} -dbtype nucl -entry "$ENTR" -range "$RANGE" -strand "$STR" -out ./Genome_fastas/${FILE}_out.fa 
     done 
-done < blast_names.txt
+done
