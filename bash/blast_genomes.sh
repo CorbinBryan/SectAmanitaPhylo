8e39ff2c41b2751ac6ef33e97324123e1f2983bb
CorbinBryan <bryan7@wisc.edu>
2023-02-15 11:09:34 -0600

    updated my blast_genomes.sh and genbank_dl.sh scripts

--- bash/blast_genomes.sh
+++ bash/blast_genomes.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 # a script to blast seq from four loci against 25 genomes
-# note: most unzip your assembly and have in subdirectory 
 # Chris, if you're reading this then change 4Loci_for_blast.fa to [MATINGTYPES].fa
 
-FILE=$1 
-
-if [ ! -d ./blastOut ]; then
-    mkdir ./blastOut; 
-fi
-if [ ! -d miniconda3 ]; then
-    cp /staging/bryan7/blast.tar.gz .
-    chmod 644 blast.tar.gz
-    tar -zxvf blast.tar.gz
-fi
-if [ ! -d ./assemblies]; then
-    tar -zxvf assemblies.tar.gz
-fi 
+for FILE in $(ls ./assemblies | cat); do
     makeblastdb -in ./assemblies/"$FILE" -dbtype nucl
     blastn -db ./assemblies/"$FILE" -query 4Loci_for_blast.fa -outfmt 6 -max_target_seqs 2 -num_threads 4 -out ./blastOut/"$FILE"_hits.txt
-
-if [ $(ls ./blastOut | wc -l) == 25 ]; then
-    tar -czvf blastOut.tar.gz ./blastOut
-    mv blastOut.tar.gz /staging/bryan7/
-fi
+done
--- bash/genbank_dl.sh
+++ bash/genbank_dl.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # A script to download files from genbank given accession nos. 
 
-for FILE in $(ls ~/SectAmanitaPhylo/RawData/GB_acc_no); do 
-#    touch ${FILE}.fa
-    epost -db nuccore -input ~/SectAmanitaPhlyo/RawData/GB_acc_no/${FILE} -format acc | efetch -format fasta > ${FILE}.fa
-    if 
-done 
+for FILE in $(ls ./GB_acc_no/*_GenBank); do
+    epost -db nuccore -input "$FILE" | efetch -format fasta > "$FILE".fa
+done
