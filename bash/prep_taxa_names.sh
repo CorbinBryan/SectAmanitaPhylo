6874ad68596aec4fdd7949be16f4e920a874ab1f
CorbinBryan <bryan7@wisc.edu>
2023-02-18 11:05:31 -0600

    updated pre_taxa_names.sh script (removed turnary (sp?) operators in favor of simple if-else if conditionals

--- bash/prep_taxa_names.sh
+++ bash/prep_taxa_names.sh
@@ -1,5 +1,17 @@
-
+# #!/bin/bash
+# # filters fasta heads from NCBI blast. for use later in tree building (i.e., coloring taxa based on species ID)
 
 awk '/>/ {
-    if ($3=="sp.") print $1,$2,$3,$4; else if ($3=="muscaria" && ($4=="var." || $4=="subsp.")) print $1,$2,$3,$4,$5; else print $1,$2,$3
-    }' ${1}
+if($3=="sp.") 
+    {
+    print $1"\t"$2,$3,$4
+    }
+else if($3=="muscaria" && ($4=="var." || $4=="subsp.")) 
+    {
+    print $1"\t"$2,$3,$4,$5
+    }
+else
+    {
+    print $1"\t"$2,$3
+    }
+}' ${1}
