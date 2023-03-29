ce123b8255cb5f6299b24d4284f8c2ee3b63928c
CorbinBryan <bryan7@wisc.edu>
2023-03-29 11:52:21 -0500

    added a brief description of algorithms, limitations, and advantages of both software packages.

--- bash/ML.sh
+++ bash/ML.sh
@@ -5,13 +5,23 @@
 
 MSA="$1"
 
-# Note IQTree model selector suggests: K3Pu+F+R3; nevertheless, I've chosen the general time reversible model for the RAxML run
+# Note 1: IQTree model selector suggests: K3Pu+F+R3; nevertheless, I've chosen the general time reversible model for the RAxML run
 #   with different rates (i.e., +G = Gama Distribution for independent rate estimates throughout diff. lineages), mostly for to 
 #   compare the outputs from the two models. Moreover, I've chosen the --bs-metric flag, which will be left at it's default value, 
 #   which is fbp, Felsenstein bootstrap. 
 
 # I've had good luck with default IQtree params on past runs with similar data. For the time being, I'll stick with these, unless 
-#   the output is particularly bad. 
+#   the output is nonsensical. 
+
+# Note that both assume your input is a high quality alignment. RAxML-NG is more accurate than IQTree2, though IQTree2 does 
+#   include added functionality (i.e., model finder) relative to RAxML-NG (and IQTree's original release). However, neither 
+#   are guaranteed to give the ctual tree, and may both get stuck in local optima. IQTree2 tries to get around this by using 
+#   multiple starting trees (which RAxML also uses) and random NNI perturbations. 
+
+# Search algorithms differ in both. As mentioned, IQTree primarily uses NNI, whereas RAxML-NG primarily uses SPR moves to search 
+#   tree space. Some believe that NNI moves are less likely to find the actual ML tree. In their original release, the authors note 
+#   that this doesn't seem to hinder IQTree. Nevertheless, it should be taken into account here. At any rate, RAxML-NG appears to be 
+#   more accurate than IQTree2. 
 
 # STEP 1: Check if data is appropriate for RAxML-NG
 raxml-ng --msa "$MSA".fasta \
