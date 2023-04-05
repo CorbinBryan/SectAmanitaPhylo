c36f2dbdf329e7910f690baaa13256a3e69eed3d
CorbinBryan <bryan7@wisc.edu>
2023-04-05 10:29:34 -0500

    altered prep_taxa_names.sh to prepare all taxa names for downstream analysis rather than for visualizaiton purposes

--- bash/prep_taxa_names.sh
+++ bash/prep_taxa_names.sh
@@ -1,17 +1,21 @@
 # #!/bin/bash
-# # filters fasta heads from NCBI blast. for use later in tree building (i.e., coloring taxa based on species ID)
+# # filters fasta heads from NCBI blast. for use later in tree building (i.e., coloring taxa based on species ID), also prepping 
+# # for ML 
 
-awk '/>/ {
+
+awk '/>/ BEGIN{OFS = "_"}{
 if($3=="sp.") 
     {
-    print $1"\t"$2,$3,$4
+    name=$1,$2,$3,$4
     }
 else if($3=="muscaria" && ($4=="var." || $4=="subsp.")) 
     {
-    print $1"\t"$2,$3,$4,$5
+    name=$1,$2,$3,$4,$5
     }
 else
     {
-    print $1"\t"$2,$3
+    name=$1,$2,$3
     }
-}' ${1}
+}{
+    sub(/>/, name)
+}' ${1} > name_prep_${1}
