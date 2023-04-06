03c90574d0d2d0540d4daa0f384dee4c7802928b
CorbinBryan <bryan7@wisc.edu>
2023-04-06 11:34:03 -0500

    .

--- bash/remove_taxa.sh
+++ bash/remove_taxa.sh
@@ -5,6 +5,8 @@
 
 # taxa to be removed must be put into a seperate file, taxa_removed.txt
 
+# $1 is argument for locus 
+
 while read ID; do
     seqkit grep -rvip "^${ID}" ${1}
-done < taxa_removed.txt
+done < ${1}_taxa_removed.txt
